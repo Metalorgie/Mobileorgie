3437f2936c13d201f97d35a8106856790fe8c881
Bacteries <e.cambray@doyoubuzz.com>
2015-04-25 17:41:15 +0200

    Add rating on review. Upgrade ionic.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('MetalorgieMobile', ['ionic', 'config', 'ngCordova', 'MetalorgieMobile.controllers', 'MetalorgieMobile.services', 'MetalorgieMobile.filters'])
+angular.module('MetalorgieMobile', ['ionic', 'config', 'ngCordova', 'MetalorgieMobile.controllers', 'MetalorgieMobile.services', 'MetalorgieMobile.filters', 'ionic.rating'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
